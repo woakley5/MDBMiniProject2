0863729a1fa286a7b91a20b332a09b09edc729e2
Will Oakley <woakley5@icloud.com>
2018-02-12 21:34:51 -0800

    Renamed file

--- Pokedex/Pokedex/MainViewController.swift
+++ Pokedex/Pokedex/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Pokedex
 //
 //  Created by SAMEER SURESH on 9/25/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
